077a1bc3d6587a1fabfff93b51a7c70b6a5f4e77
MaistoMagnetas <mindaugascel2@gmail.com>
2018-09-23 19:55:47 +0300

    Updated input/output fields availabiblity

--- app/src/main/java/lovedice/cactustree/pc/morsecode/MainActivity.java
+++ app/src/main/java/lovedice/cactustree/pc/morsecode/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText morseCodeInput, morseCodeOutput;
     private Button btnToText, btnToLight, btnToVibration, btnToSound;
+    private Button btnMorseSpace, btnMorseDot, btnMorseDash, btnMorseWordEnd, btnMorseDel;
+    private CheckBox btnMorseSwitchMode;
+    private boolean isMorseModeEnabled = false;
 
     private VibrationMorse vibrationMorse;
     private String inputString;
@@ -28,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initXMLItems();
+        changeStatusOfButtons(isMorseModeEnabled); //Disable buttons on app load
 
         btnToText.setOnClickListener(new View.OnClickListener() { //Morse to text
             @Override
@@ -92,15 +99,123 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        btnMorseDash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextOnMorseButtonClick("-");
+            }
+        });
+
+        btnMorseDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextOnMorseButtonClick("•");
+
+            }
+        });
+
+        btnMorseSpace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextOnMorseButtonClick(" ");
+
+            }
+        });
+
+        btnMorseWordEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextOnMorseButtonClick("/");
+
+            }
+        });
+
+        btnMorseSwitchMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                changeStatusOfMode();
+                clearTextsOnModeChanged();
+                changeStatusOfButtons(isMorseModeEnabled);
+
+            }
+        });
+
+        btnMorseDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stringBefore = morseCodeOutput.getText().toString();
+                int stringSize = stringBefore.length();
+                if (stringSize > 0) {
+                    morseCodeOutput.setText(stringBefore.substring(0, stringBefore.length() - 1));
+                }
+            }
+        });
+    }
+
+    /**
+     * Changes status of morse or text input
+     */
+    private void changeStatusOfMode() {
+        isMorseModeEnabled ^= true;
+    }
+
+    /**
+     * Clears input/ output text fields data after mode changed.
+     */
+    private void clearTextsOnModeChanged()
+    {
+        morseCodeOutput.setText("");
+        morseCodeInput.setText("");
+    }
+
+    /**
+     * Enables buttons if morse mode is turned on
+     * @param status status to enable or dissable buttons.
+     */
+    private void changeStatusOfButtons(boolean status) {
+        if (isMorseModeEnabled) {
+            btnMorseDel.setEnabled(status);
+            btnMorseWordEnd.setEnabled(status);
+            btnMorseDot.setEnabled(status);
+            btnMorseDash.setEnabled(status);
+            btnMorseSpace.setEnabled(status);
+            morseCodeInput.setInputType(InputType.TYPE_NULL);
+        } else {
+            btnMorseDel.setEnabled(status);
+            btnMorseWordEnd.setEnabled(status);
+            btnMorseDot.setEnabled(status);
+            btnMorseDash.setEnabled(status);
+            btnMorseSpace.setEnabled(status);
+            morseCodeInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        }
+    }
+
+    /**
+     * Sets text to morse input text area.
+     * @param type type => dot, dash, space etc.
+     */
+    private void setTextOnMorseButtonClick(String type) {
+        String stringBefore = morseCodeOutput.getText().toString();
+        morseCodeOutput.setText(stringBefore + type);
     }
 
     private void initXMLItems() {
+        //Text
         morseCodeInput = findViewById(R.id.morse_text_input);
         morseCodeOutput = findViewById(R.id.morse_text_output);
         btnToLight = findViewById(R.id.morse_button_light);
         btnToSound = findViewById(R.id.morse_button_sound);
         btnToText = findViewById(R.id.morse_button_text);
         btnToVibration = findViewById(R.id.morse_button_vibration);
+        morseCodeOutput.setInputType(InputType.TYPE_NULL);
+        //Morse
+        btnMorseDash = findViewById(R.id.morse_button_dash);
+        btnMorseDot = findViewById(R.id.morse_button_dot);
+        btnMorseSpace = findViewById(R.id.morse_button_space);
+        btnMorseWordEnd = findViewById(R.id.morse_button_wordend);
+        btnMorseDel = findViewById(R.id.morse_button_remove);
+        btnMorseSwitchMode = findViewById(R.id.morse_button_switchmode);
     }
 
     private boolean isStringNotEmpty(String inputString) {
